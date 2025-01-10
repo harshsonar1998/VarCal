e0ebcc9cd23c75bba57232bb9d6f7a8287dc1f41
harshsonar1998 <33555889+harshsonar1998@users.noreply.github.com>
2025-01-10 18:33:42 -0500

    Update pracJava.java

--- loopPrac.java
+++ loopPrac.java
@@ -1,159 +1,156 @@
-package javaPrac;
+package pracJava;
 import java.util.Scanner;
 
-public class loopPrac {
+public class practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		greet();// To invoke the methods to main and to run.
+		greeting();
 		//multi();
-		//oddS(); 
-		//fac();
-		//odds();
-		//lcm();
-		//gcd();
-		//prime();
+		//System.out.println(oddS());
+		//System.out.println(fac());
+		//System.out.println(lcm());
+		//System.out.println(prime());
+		//System.out.print(sum());
 		//reverse();
-		//fb();
-		//pat1();
-		//pat2();
-		//pat3();
-		pali();
+		//pali();
+		//fibo();
+		
+
 	}
-	public static void greet() {
-		System.out.println("Welcome to the Quantum Mechanics Advance Calculator.");
+	public  static void greeting() {
+		System.out.println("Welcome to the Java Practice Session.");
 	}
 	public static void multi() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number to display multiplication table: ");
-		int num = input.nextInt();
-		int i =1; // Initialization that multiplication table will starts from 1
-		while (i<=10) { // the loop will run less than and equal to 10 times for the multiplication table till 10
-			System.out.println(num*i); // (num*i) due to the first the number will be entered by the person, then enter the loop and will multiply by 1 as i is set to be 1, then loop  exit and then again enter the loop sees second line then multiply by 2 and goes on.
+		System.out.print("Enter the number for the multiplication table: ");
+		int num =  input.nextInt();
+		int i =1;
+		while (i<=10) {
+			System.out.println(num*i);
 			i++;
 		}
 	}
 	public static int oddS() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number till the summation of odd numbers: ");
+		System.out.print("Enter the number till you want the summation: ");
 		int num = input.nextInt();
-		int sum =0; // summation of all odd numbers will starts from zero so 0 is set.
-		int i=1; // odd number starts from 1 so Initialization starts from here. 
-		while(i<=num) { // The loop  will run till the i will less than or equal to the entered number.
-			sum +=i; // sum will keep the number addition till the loops end
-			System.out.println(sum);
-			i+=2; // loop will skip two number and then run until it reaches the conidtion.
+		int sum =0;
+		int i =1;
+		while (i<=num) {
+			sum+=i;
+			i+=2;
 		}
 		return sum;
 	}
 	public static long fac() {
-		Scanner input = new  Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the number for the factorial: ");
 		long num = input.nextLong();
-		long sum = 1; // Multiplier loop.
-		long i = 1; // I have taken 1 due to loop  multiplies by 1 if started from 0, then the output will be 0.
-		while (i<=num) { // The loop  will run till the i will less than or equal to the entered number.
-			sum*=i; // It multiplies the current value of sum by i and updates sum with the result.
-			System.out.println(sum);
-			i++;	// the iteration will run until the condition is satisfied.		
+		long facto =1;
+		int i =1;
+		while (i<=num) {
+			facto *= i;
+			i++;
 		}
-		return sum;
-	}
-	public static int odds() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number for the summation: ");
-		int num = input.nextInt();
-		int sum =0; // storage of number will be 0.
-		while (num > 0) { // condition = run the loop till the number divisible by 10 is > 0.
-			sum += num % 10; // Apply modulus due to finding out the last digit by diving by 10 and stored in the sum. there is an addition sign so the loop continues till the num is more  than zero and stores the num and addition.
-			num /=10; // again divide by 10 to get the number and stored in sum.
-			System.out.println(sum);
-		}
-		return sum;
+		return facto;
 	}
 	public static int lcm() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the 1st number for the LCM: ");
 		int f = input.nextInt();
-		System.out.print("Enter the 2nd number for the LCM: ");
+		System.out.print("Enter the 2st number for the LCM: ");
 		int s = input.nextInt();
-		int i = 1;
-		while (true) { // True = written because of the infinite loop and it will run until the the loop is satisfied and break.
-			int mul = f*i; // multiplies with the entered 1st number.
-			if (mul % s ==0) { // check weather the mul is divisible by second, if answer 0 then condition satisfied, if answer not 0, then loop continues.
-				System.out.print(mul);
-				return mul;
-			}
-			i++; // increments till the loop is satisfied.
-		}
-		
-	}
-	public static int prime() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter number to check: ");
-		int num = input.nextInt();
-		int i =2; // the prime number means the number divisible by their own or by 1, so here starting from 2. This is the initialization.
-		while (i<num) { // condition that loop will run until the entered number.
-			if (num % i ==0) { // another condition where the entered number has any factor that give us 0, will be stored in the i.
-			System.out.print(i);	
+		int i =1;
+		while(true) {
+			int factor = f*i;
+			if (factor % s ==0) {
+				return factor;
 			}
 			i++;
 		}
-		return i;
-	}
-	public static int reverse() {
+			
+		
+		}
+	public static boolean prime() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number to be reversed: ");
+		System.out.print("Enter the number to check for the prime number: ");
 		int num = input.nextInt();
-		int sum = 0; // initial value for the storage.
-		while (num>0) { // condition to run the loop till the num value is greater than 0.
-			sum = num % 10; // to find out the last digit of the entered number.
-			num /=10; // again to find out the second, third...n last till the num value is >0.
-			System.out.print(sum);
+		int i =2;
+		while (i<num) {
+			if (num % i ==0) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+	public static int sum() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the nuber for the summation: ");
+		int num = input.nextInt();
+		int sum =0;
+		while (num>0) {
+			int digit = num%10;
+			sum += digit;
+			num /=10;
 		}
 		return sum;
 	}
-	public static int fb() {
+	public static void reverse() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number till for Fibonacci series: ");
+		System.out.print("Enter the number to be reversed: ");
 		int num = input.nextInt();
-		int sum = 1;// 1st digit of fibonacci
-		int i=0; // initialization
-		while (i+sum<=num) { // condition =  run the loop till the two numbers are less than the entered number.
-			int fl = sum + i; // addition of two number.
-			sum = i; // swap from 1st to second
-			i = fl; // and swap of second to third
-			System.out.println(fl);
+		int sum = 0;
+		while (num>0) {
+			int digit = num %10;
+			sum = digit;
+			num/=10;
+			System.out.print(sum);
 		}
-		return i;
-	}
-	public static void pat1() {
-		System.out.println("*\n**\n***\n****\n*****");
-	}
-	public static void pat2() {
-		System.out.println("*****\n****\n***\n**\n*");
-	}
-	public static void pat3() {
-		System.out.println("    *\n   **\n  ***\n ****\n*****");
 	}
 	public static void pali() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number to test palindrome: ");
+		Scanner input =  new Scanner(System.in);
+		System.out.print("Enter the numbe for know the palindrome number: ");
 		int num = input.nextInt();
-		int ov = num; // used to store the original number for the comparison.
-		int rev =0; // storage for the assignment operator.
-		while (num >0) { // loop will run till the entered number is more than zero and stop if less than zero.
-			int digit = num % 10; // used to extract the last digit.
-			rev =rev *10 + digit; // the stored rev * 10 + last currently extracted and goes on. ex. if 123, 0*10+3 = rev =3. 3*10+2 = rev = 32. 32*10 +1 = rev = 321. (123 !=321) will not be a palindrome number.
-			num /=10; // divides the remaning digits.
-		}
-		if (rev==ov) {
-			System.out.println(ov + " palindrome number.");
+		int ov=num;
+		int sum =0;
+		while (num>0) {
+			int digit = num % 10;
+			sum = sum*10+digit;
+			num/=10;
+			}
+		if (ov==sum) {
+			System.out.println("The entered number is a palindrome number.");
 		}
 		else {
-			System.out.println( ov + " not a palindrome number.");
+			System.out.println("The entered number is not a palindrome number.");
 		}
+		}
+	public static void fibo() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number up to which the Fibonacci series should be printed: ");
+	    int num = input.nextInt();
+	    int a=0;
+	    int b=1;
+	    System.out.println(a);
+	    
+	    if (num>=1) {
+	    	System.out.println(b);
+	    }
+	    while (a+b<=num) {
+	    	int next = a+b;
+	    	System.out.println(next);
+	    	a=b;
+	    	b=next;
+	    }
+
+		        
+		    }
+
 	}
 	
-}
+	
+	
+	
 
